9808f3645f8a9ae33bff90911fbd9d83bbdd19b5
SSD Matt <matthecrazyguy@gmail.com>
2024-02-17 16:38:46 -0500

    reorganized

--- spring2024.sql
+++ spring2024.sql
@@ -10,8 +10,8 @@ CREATE TABLE major_maps (
         AND major_factor <= 1
     )
 );
-INSERT INTO major_maps VALUES('g',0.625);
 INSERT INTO major_maps VALUES('m',1.0);
+INSERT INTO major_maps VALUES('g',0.625);
 CREATE TABLE classes (
     class_name TEXT PRIMARY KEY,
     major_state TEXT NOT NULL,
@@ -21,16 +21,16 @@ CREATE TABLE classes (
 INSERT INTO classes VALUES('cmsc351','m',600);
 INSERT INTO classes VALUES('cmsc330','m',100);
 CREATE TABLE lp_templates (late_policy_name TEXT PRIMARY KEY);
-INSERT INTO lp_templates VALUES('5x3');
 INSERT INTO lp_templates VALUES('stand');
+INSERT INTO lp_templates VALUES('10x1');
 CREATE TABLE lp_template_deadvars (
     late_policy_name TEXT,
     deadline_variable TEXT,
     FOREIGN KEY (late_policy_name) REFERENCES lp_templates (late_policy_name) ON UPDATE CASCADE,
     PRIMARY KEY (late_policy_name, deadline_variable)
 );
-INSERT INTO lp_template_deadvars VALUES('5x3','x1');
-INSERT INTO lp_template_deadvars VALUES('stand','x1');
+INSERT INTO lp_template_deadvars VALUES('stand','x0');
+INSERT INTO lp_template_deadvars VALUES('10x1','x0');
 CREATE TABLE lp_template_deadvar_phases (
     late_policy_name TEXT,
     phase_value REAL CHECK (
@@ -42,39 +42,53 @@ CREATE TABLE lp_template_deadvar_phases (
     FOREIGN KEY (late_policy_name, deadline_variable) REFERENCES lp_template_deadvars (late_policy_name, deadline_variable) ON UPDATE CASCADE,
     PRIMARY KEY (late_policy_name, deadline_variable, hour_offset)
 );
-INSERT INTO lp_template_deadvar_phases VALUES('5x3',0.050000000000000002775,'x1',0);
-INSERT INTO lp_template_deadvar_phases VALUES('5x3',0.050000000000000002775,'x1',24);
-INSERT INTO lp_template_deadvar_phases VALUES('5x3',0.050000000000000002775,'x1',48);
-INSERT INTO lp_template_deadvar_phases VALUES('5x3',0.84999999999999997779,'x1',72);
-INSERT INTO lp_template_deadvar_phases VALUES('stand',1.0,'x1',0);
-CREATE TABLE assignments (
-    assignment_name TEXT PRIMARY KEY,
-    class_name TEXT,
-    points REAL,
-    late_policy_name TEXT,
-    FOREIGN KEY (class_name) REFERENCES classes (class_name) ON UPDATE CASCADE,
-    FOREIGN KEY (late_policy_name) REFERENCES lp_templates (late_policy_name) ON UPDATE CASCADE
-);
-CREATE TABLE deadvar_maps (
-    assignment_name TEXT,
-    late_policy_name TEXT,
-    deadline_variable TEXT,
-    deadline_instance TEXT NOT NULL,
-    FOREIGN KEY (assignment_name) REFERENCES assignments (assignment_name) ON UPDATE CASCADE,
-    FOREIGN KEY (late_policy_name, deadline_variable) REFERENCES lp_template_deadvars (late_policy_name, deadline_variable) ON UPDATE CASCADE,
-    PRIMARY KEY (
-        assignment_name,
-        late_policy_name,
-        deadline_variable
-    )
-);
+INSERT INTO lp_template_deadvar_phases VALUES('stand',1.0,'x0',0);
+INSERT INTO lp_template_deadvar_phases VALUES('10x1',0.1,'x0',0);
+INSERT INTO lp_template_deadvar_phases VALUES('10x1',0.9,'x0',24);
 CREATE TABLE assignment_templates (
     assignment_type TEXT,
     class_name TEXT,
     points REAL,
     late_policy_name TEXT,
+    commute_factor REAL CHECK (
+        0 < commute_factor
+        AND commute_factor <= 1
+    ),
     FOREIGN KEY (class_name) REFERENCES classes (class_name) ON UPDATE CASCADE,
-    FOREIGN KEY (late_policy_name) REFERENCES lp_templates (late_policy_name) ON UPDATE CASCADE PRIMARY KEY (assignment_type, class_name)
+    FOREIGN KEY (late_policy_name) REFERENCES lp_templates (late_policy_name) ON UPDATE CASCADE,
+    PRIMARY KEY (assignment_type, class_name)
 );
-INSERT INTO assignment_templates VALUES('hw','cmsc351',9.0909090909090917165,'stand');
+INSERT INTO assignment_templates VALUES('proj','cmsc330',NULL,'10x1',0.6);
+INSERT INTO assignment_templates VALUES('hw','cmsc351',11.111111111111111604,'stand',0.25);
+CREATE TABLE assignments (
+    assignment_name TEXT,
+    class_name TEXT,
+    points REAL,
+    late_policy_name TEXT,
+    commute_factor REAL CHECK (
+        0 < commute_factor
+        AND commute_factor <= 1
+    ),
+    template TEXT,
+    FOREIGN KEY (class_name) REFERENCES classes (class_name) ON UPDATE CASCADE,
+    FOREIGN KEY (late_policy_name) REFERENCES lp_templates (late_policy_name) ON UPDATE CASCADE,
+    FOREIGN KEY (template, class_name) REFERENCES assignment_templates (assignment_type, class_name) ON UPDATE CASCADE,
+    PRIMARY KEY (assignment_name, class_name)
+);
+INSERT INTO assignments VALUES('proj2','cmsc330',5.0,NULL,NULL,'proj');
+INSERT INTO assignments VALUES('proj1','cmsc330',30.0,'stand',NULL,'proj');
+CREATE TABLE deadvar_maps (
+    assignment_name TEXT,
+    class_name TEXT,
+    deadline_variable TEXT,
+    deadline_instance TEXT NOT NULL,
+    FOREIGN KEY (assignment_name, class_name) REFERENCES assignments (assignment_name, class_name) ON DELETE CASCADE,
+    PRIMARY KEY (
+        assignment_name,
+        class_name,
+        deadline_variable
+    )
+);
+INSERT INTO deadvar_maps VALUES('proj2','cmsc330','x0','2024-02-27 23:59:00');
+INSERT INTO deadvar_maps VALUES('proj1','cmsc330','x0','2024-05-10 23:59:00');
 COMMIT;
