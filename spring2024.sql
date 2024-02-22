d585865781052bfadb6b7322ad38536b65ad1877
Matt Lyapin <matthecrazyguy@gmail.com>
2024-02-22 11:24:29 -0500

    added on update cascade condition on assignment_deadvar_maps and template_deadvar_maps. this doesn't actually change anything yet because hwopt has no modify/delete assignment/template functionality but it will save a few headaches when and if we decide to implement it

--- spring2024.sql
+++ spring2024.sql
@@ -9,9 +9,9 @@ CREATE TABLE major_maps (
     starting_offset REAL NOT NULL
 );
 INSERT INTO major_maps
-VALUES('m', 70.0, 0.4);
+VALUES('m', 70.0, 0.4000000000000000222);
 INSERT INTO major_maps
-VALUES('g', 60.0, 0.2);
+VALUES('g', 60.0, 0.2000000000000000111);
 CREATE TABLE classes (
     class_name TEXT PRIMARY KEY,
     major_state TEXT NOT NULL,
@@ -63,23 +63,23 @@ CREATE TABLE lp_template_deadvar_phases (
     PRIMARY KEY (late_policy_name, deadvar, hour_offset)
 );
 INSERT INTO lp_template_deadvar_phases
-VALUES('5x3', 0.05, 0, 0);
+VALUES('5x3', 0.050000000000000002775, 0, 0);
 INSERT INTO lp_template_deadvar_phases
-VALUES('5x3', 0.05, 0, 24);
+VALUES('5x3', 0.050000000000000002775, 0, 24);
 INSERT INTO lp_template_deadvar_phases
-VALUES('5x3', 0.05, 0, 48);
+VALUES('5x3', 0.050000000000000002775, 0, 48);
 INSERT INTO lp_template_deadvar_phases
-VALUES('5x3', 0.85, 0, 72);
+VALUES('5x3', 0.84999999999999997779, 0, 72);
 INSERT INTO lp_template_deadvar_phases
 VALUES('stand', 1.0, 0, 0);
 INSERT INTO lp_template_deadvar_phases
-VALUES('10x1', 0.1, 0, 0);
+VALUES('10x1', 0.10000000000000000555, 0, 0);
 INSERT INTO lp_template_deadvar_phases
-VALUES('10x1', 0.9, 0, 24);
+VALUES('10x1', 0.9000000000000000222, 0, 24);
 INSERT INTO lp_template_deadvar_phases
-VALUES('5xD', 0.05, 0, 0);
+VALUES('5xD', 0.050000000000000002775, 0, 0);
 INSERT INTO lp_template_deadvar_phases
-VALUES('5xD', 0.95, 1, 0);
+VALUES('5xD', 0.94999999999999995559, 1, 0);
 CREATE TABLE assignment_templates (
     assignment_type TEXT,
     class_name TEXT,
@@ -97,18 +97,42 @@ INSERT INTO assignment_templates
 VALUES(
         'hw',
         'cmsc351',
-        11.111111111111111604,
+        11.111111111111112492,
         'stand',
         0.25
     );
 INSERT INTO assignment_templates
-VALUES('midterm', 'cmsc351', 100.0, 'stand', 0.1);
+VALUES(
+        'midterm',
+        'cmsc351',
+        100.0,
+        'stand',
+        0.10000000000000000555
+    );
 INSERT INTO assignment_templates
-VALUES('proj', 'cmsc330', NULL, '10x1', 0.6);
+VALUES(
+        'proj',
+        'cmsc330',
+        NULL,
+        '10x1',
+        0.59999999999999997779
+    );
 INSERT INTO assignment_templates
-VALUES('midterm', 'cmsc330', 12.0, 'stand', 0.1);
+VALUES(
+        'midterm',
+        'cmsc330',
+        12.0,
+        'stand',
+        0.10000000000000000555
+    );
 INSERT INTO assignment_templates
-VALUES('quiz', 'cmsc330', 2.5, 'stand', 0.1);
+VALUES(
+        'quiz',
+        'cmsc330',
+        2.5,
+        'stand',
+        0.10000000000000000555
+    );
 INSERT INTO assignment_templates
 VALUES(
         'hw',
@@ -118,13 +142,33 @@ VALUES(
         0.25
     );
 INSERT INTO assignment_templates
-VALUES('midterm', 'stat410', 15.0, 'stand', 0.1);
+VALUES(
+        'midterm',
+        'stat410',
+        15.0,
+        'stand',
+        0.10000000000000000555
+    );
 INSERT INTO assignment_templates
-VALUES('sa', 'psyc100', 50.0, '5xD', 0.1);
+VALUES(
+        'sa',
+        'psyc100',
+        50.0,
+        '5xD',
+        0.10000000000000000555
+    );
 INSERT INTO assignment_templates
-VALUES('midterm', 'psyc100', 75.0, 'stand', 0.1);
+VALUES(
+        'midterm',
+        'psyc100',
+        75.0,
+        'stand',
+        0.10000000000000000555
+    );
 INSERT INTO assignment_templates
 VALUES('lc', 'psyc100', 5.0, 'stand', 0.0);
+INSERT INTO assignment_templates
+VALUES('ws', 'psyc100', NULL, '5xD', 0.0);
 CREATE TABLE template_deadvar_maps (
     template TEXT,
     class_name TEXT,
@@ -132,7 +176,7 @@ CREATE TABLE template_deadvar_maps (
     deadline_date TEXT,
     deadline_hour INT,
     deadline_min INT,
-    FOREIGN KEY (template, class_name) REFERENCES assignment_templates (assignment_type, class_name) ON DELETE CASCADE,
+    FOREIGN KEY (template, class_name) REFERENCES assignment_templates (assignment_type, class_name) ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY (
         template,
         class_name,
@@ -168,6 +212,17 @@ INSERT INTO template_deadvar_maps
 VALUES('midterm', 'psyc100', 0, NULL, 22, 50);
 INSERT INTO template_deadvar_maps
 VALUES('lc', 'psyc100', 0, NULL, 23, 59);
+INSERT INTO template_deadvar_maps
+VALUES('ws', 'psyc100', 0, NULL, 23, 59);
+INSERT INTO template_deadvar_maps
+VALUES(
+        'ws',
+        'psyc100',
+        1,
+        '2024-05-13 00:00:00',
+        23,
+        59
+    );
 CREATE TABLE assignments (
     assignment_name TEXT,
     class_name TEXT,
@@ -199,7 +254,7 @@ VALUES(
         NULL,
         NULL,
         'midterm',
-        0,
+        0.0,
         0
     );
 INSERT INTO assignments
@@ -210,7 +265,7 @@ VALUES(
         NULL,
         NULL,
         'proj',
-        0,
+        0.0,
         0
     );
 INSERT INTO assignments
@@ -221,7 +276,7 @@ VALUES(
         NULL,
         NULL,
         'midterm',
-        0,
+        0.0,
         0
     );
 INSERT INTO assignments
@@ -232,31 +287,13 @@ VALUES(
         NULL,
         NULL,
         'quiz',
-        0,
+        0.0,
         0
     );
 INSERT INTO assignments
-VALUES(
-        'hw4',
-        'cmsc351',
-        NULL,
-        NULL,
-        NULL,
-        'hw',
-        0,
-        0
-    );
+VALUES('hw4', 'cmsc351', NULL, NULL, NULL, 'hw', 0.0, 0);
 INSERT INTO assignments
-VALUES(
-        'hw3',
-        'stat410',
-        NULL,
-        NULL,
-        NULL,
-        'hw',
-        0,
-        0
-    );
+VALUES('hw3', 'stat410', NULL, NULL, NULL, 'hw', 0.0, 0);
 INSERT INTO assignments
 VALUES(
         'midterm1',
@@ -265,18 +302,7 @@ VALUES(
         NULL,
         NULL,
         'midterm',
-        0,
-        0
-    );
-INSERT INTO assignments
-VALUES(
-        'proj1',
-        'cmsc330',
-        30.0,
-        'stand',
-        NULL,
-        'proj',
-        0,
+        0.0,
         0
     );
 INSERT INTO assignments
@@ -287,7 +313,7 @@ VALUES(
         NULL,
         NULL,
         'sa',
-        0,
+        5.0,
         0
     );
 INSERT INTO assignments
@@ -298,7 +324,106 @@ VALUES(
         NULL,
         NULL,
         'sa',
-        0,
+        0.0,
+        0
+    );
+INSERT INTO assignments
+VALUES(
+        'quiz1',
+        'cmsc330',
+        NULL,
+        NULL,
+        NULL,
+        'quiz',
+        30.000000000000003551,
+        1
+    );
+INSERT INTO assignments
+VALUES(
+        'hw1',
+        'cmsc351',
+        NULL,
+        NULL,
+        NULL,
+        'hw',
+        13.0,
+        1
+    );
+INSERT INTO assignments
+VALUES(
+        'lc-01',
+        'psyc100',
+        NULL,
+        NULL,
+        NULL,
+        'lc',
+        100.0,
+        0
+    );
+INSERT INTO assignments
+VALUES(
+        'lc-02',
+        'psyc100',
+        NULL,
+        NULL,
+        NULL,
+        'lc',
+        100.0,
+        0
+    );
+INSERT INTO assignments
+VALUES(
+        'lc-03',
+        'psyc100',
+        NULL,
+        NULL,
+        NULL,
+        'lc',
+        100.0,
+        0
+    );
+INSERT INTO assignments
+VALUES(
+        'ws-03',
+        'psyc100',
+        38.0,
+        NULL,
+        NULL,
+        'ws',
+        5.0,
+        0
+    );
+INSERT INTO assignments
+VALUES(
+        'ws-04',
+        'psyc100',
+        34.0,
+        NULL,
+        NULL,
+        'ws',
+        5.0,
+        0
+    );
+INSERT INTO assignments
+VALUES(
+        'midterm1',
+        'psyc100',
+        NULL,
+        NULL,
+        NULL,
+        'midterm',
+        0.0,
+        0
+    );
+INSERT INTO assignments
+VALUES(
+        'ws-05',
+        'psyc100',
+        20.0,
+        NULL,
+        NULL,
+        'ws',
+        0.0,
         0
     );
 CREATE TABLE assignment_deadvar_maps (
@@ -308,7 +433,7 @@ CREATE TABLE assignment_deadvar_maps (
     deadline_date TEXT,
     deadline_hour INT,
     deadline_min INT,
-    FOREIGN KEY (assignment_name, class_name) REFERENCES assignments (assignment_name, class_name) ON DELETE CASCADE,
+    FOREIGN KEY (assignment_name, class_name) REFERENCES assignments (assignment_name, class_name) ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY (
         assignment_name,
         class_name,
@@ -380,15 +505,6 @@ VALUES(
     );
 INSERT INTO assignment_deadvar_maps
 VALUES(
-        'proj1',
-        'cmsc330',
-        0,
-        '2024-05-10 00:00:00',
-        NULL,
-        NULL
-    );
-INSERT INTO assignment_deadvar_maps
-VALUES(
         'sa-01',
         'psyc100',
         0,
@@ -402,6 +518,87 @@ VALUES(
         'psyc100',
         0,
         '2024-02-23 00:00:00',
+        NULL,
+        NULL
+    );
+INSERT INTO assignment_deadvar_maps
+VALUES(
+        'quiz1',
+        'cmsc330',
+        0,
+        '2024-02-09 00:00:00',
+        NULL,
+        NULL
+    );
+INSERT INTO assignment_deadvar_maps
+VALUES(
+        'hw1',
+        'cmsc351',
+        0,
+        '2024-01-31 00:00:00',
+        NULL,
+        NULL
+    );
+INSERT INTO assignment_deadvar_maps
+VALUES(
+        'lc-01',
+        'psyc100',
+        0,
+        '2024-01-31 00:00:00',
+        NULL,
+        NULL
+    );
+INSERT INTO assignment_deadvar_maps
+VALUES(
+        'lc-02',
+        'psyc100',
+        0,
+        '2024-02-07 00:00:00',
+        NULL,
+        NULL
+    );
+INSERT INTO assignment_deadvar_maps
+VALUES(
+        'lc-03',
+        'psyc100',
+        0,
+        '2024-02-14 00:00:00',
+        NULL,
+        NULL
+    );
+INSERT INTO assignment_deadvar_maps
+VALUES(
+        'ws-03',
+        'psyc100',
+        0,
+        '2024-02-08 00:00:00',
+        NULL,
+        NULL
+    );
+INSERT INTO assignment_deadvar_maps
+VALUES(
+        'ws-04',
+        'psyc100',
+        0,
+        '2024-02-15 00:00:00',
+        NULL,
+        NULL
+    );
+INSERT INTO assignment_deadvar_maps
+VALUES(
+        'midterm1',
+        'psyc100',
+        0,
+        '2024-02-29 00:00:00',
+        NULL,
+        NULL
+    );
+INSERT INTO assignment_deadvar_maps
+VALUES(
+        'ws-05',
+        'psyc100',
+        0,
+        '2024-03-01 00:00:00',
         NULL,
         NULL
     );
